c3fbf6fcd6903d89c5d479d7a4ae1d98d40fce2d
Angelita Santos <angelitasantos@gmail.com>
2023-10-28 17:17:59 -0300

    connects database to project

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import admin from 'firebase-admin';
+
 
 // REST API http://api.controle-de-gastos.com/transactions
 const app = express();
 
+admin.initializeApp({
+  credential: admin.credential.cert('serviceAccountKey.json')
+});
+
+
 // GET      http://api.controle-de-gastos.com/transactions
 app.get('/transactions', (request, response) => {
-    console.log('GET transactions');
-    response.json([{id: 1}]);
+    admin.firestore()
+        .collection('transactions')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.id
+            }))
+            response.json(transactions);
+        })
 })
 
 app.listen(3000, () => console.log('API rest iniciada em http://localhost:3000'))
+
+
