a5f50fd73a41237e06652ad9d38d4926faffe36c
Angelita Santos <angelitasantos@gmail.com>
2023-10-29 08:37:00 -0300

    create models camada

---
+++ transactions/model.js
@@ -0,0 +1,34 @@
+import admin from 'firebase-admin';
+
+
+export class Transaction {
+
+    date;
+    description;
+    money;
+    transactionType;
+    type;
+    user;
+
+    findByUser() {
+        if (!this.user?.uid) {
+            return Promise.reject({
+                code: 500,
+                message: 'Usuário não informado !!!'
+            });
+        }
+        
+        return admin.firestore()
+            .collection('transactions')
+            .where('user.uid', '==', this.user.uid)
+            .orderBy('date', 'desc')
+            .get()
+            .then(snapshot => {
+                return snapshot.docs.map(doc => ({
+                    ...doc.data(),
+                    uid: doc.id
+                }))
+            })
+    }
+
+}
